e0aedf65b5df59bb67ce8d03981f15f1e8f958e3
Grigorii <grigorii.orlov@sparm.com>
2024-08-01 10:58:05 +0300

    Changes in main content 2

--- web-site/monk_site/myshop/views.py
+++ web-site/monk_site/myshop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from .models import Category, Product, Gender
 from cart.forms import CartAddProductForm
 
 
@@ -27,8 +27,22 @@ def product_detail(request, id, slug):
     return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form })
 
 
-def main(request):
-    return render(request, 'shop/main.html')
+def main(request, category_slug=None):
+    cart_product_form = CartAddProductForm()
+    category = None
+    categories = Gender.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Gender, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request, 'shop/main.html',
+                  {
+                      'category': category,
+                      'categories': categories,
+                      'products': products,
+                      'cart_product_form': cart_product_form
+                  })
 
 
 def politika(request):
